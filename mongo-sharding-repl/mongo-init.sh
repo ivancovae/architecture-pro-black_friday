e792c9dcebe77d93d769f676e5fa9028e7d1f07e
DESKTOP-D0VFTGP\Admin <ivancovae@gmail.com>
2025-11-09 01:05:44 +0300

    Задание 3. Репликация

---
+++ mongo-sharding-repl/mongo-init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+###
+# Инициализируем бд
+###
+
+docker compose exec -T mongo-sharding-repl mongosh <<EOF
+use somedb
+for(var i = 0; i < 1000; i++) db.helloDoc.insertOne({age:i, name:"ly"+i})
+EOF
+
